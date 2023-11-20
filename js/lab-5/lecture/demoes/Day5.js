bb2604580b056417a9c5a299db5a87c43b8931ae
hab <mohab.khalid.mahmoud@gmail.com>
2023-11-20 16:52:10 +0200

    lab-5

---
+++ js/lab-5/lecture/demoes/Day5.js
@@ -0,0 +1,20 @@
+// setInterval(function,time by millisecond)
+// setTimeout(function, time by miliseconds)
+
+
+
+// let id=setInterval(function(){
+//     console.log("Hi");
+// },1000);
+
+// clearInterval(id);
+
+// let id=setTimeout(function(){
+//     console.log("HI");
+// },3000);
+
+
+// clearTimeout(id);
+
+
+
