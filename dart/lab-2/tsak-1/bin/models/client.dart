85039b0bf438efdb99e6e20700f2c6c98eff2053
hab <mohab.khalid.mahmoud@gmail.com>
2023-12-27 12:24:32 +0200

    task 1 & task 2

---
+++ dart/lab-2/tsak-1/bin/models/client.dart
@@ -0,0 +1,8 @@
+import 'personal_info.dart';
+
+class Client {
+  String name;
+  PersonalInfo? personalInfo;
+
+  Client({required this.name, this.personalInfo});
+}
---
+++ dart/lab-2/tsak-1/bin/models/personal_info.dart
@@ -0,0 +1,8 @@
+import 'address.dart';
+
+class PersonalInfo {
+  String? email;
+  Address? address;
+
+  PersonalInfo({this.email, this.address});
+}
